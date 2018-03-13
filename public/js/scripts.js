dbd7066a3a6b72bb2c0f0564fd020469d5889962
Ophus Wong <ophus.wong@gmail.com>
2018-03-13 16:50:50 -0600

    Add functions to js add closure for colorBoxes add event listener for spacebar to call generateNewPalette refactor generateNewPalette

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,16 +1,58 @@
-$(document).ready(generateNewPalette());
-$(".generate-palette-btn").click(generateNewPalette);
+$(document).ready(generateNewPalette);
+$('.generate-palette-btn').click(generateNewPalette);
+$('.color-box-lock-btn').click(lockColor);
+
+$(document).keydown((event) => {
+  if (event.keyCode === 32 && document.activeElement.tagName !== "INPUT") {
+    event.preventDefault();
+    generateNewPalette();
+  }
+});
+
+const colorBoxes = (() => {
+  let boxes = [
+    'color-1',
+    'color-2',
+    'color-3',
+    'color-4',
+    'color-5',
+  ];
+
+  return {
+    getBoxes: () => {
+      return boxes;
+    },
+    lock: (id) => {
+      boxes = boxes.filter(lock => lock !== id);
+    },
+    unlock: (id) => {
+      boxes = [...boxes, id];
+    }
+  }
+})();
 
 function generateNewPalette () {
-  for (let i = 1; i < 6; i++) {
+  colorBoxes.getBoxes().forEach(box => {
     const randomHexColor = generateRandomHex();
 
-    $(`#color-${i}`).css('background-color', randomHexColor)
-    $(`#color-${i}`).text(randomHexColor.toUpperCase());
-  }
+    $(`#${box}`).css('background-color', randomHexColor)
+    $(`#${box}-text`).text(randomHexColor.toUpperCase());
+  });
 };
 
 function generateRandomHex () {
   return '#'+Math.random().toString(16).slice(-6)
+};
+
+function lockColor (event) {
+  const { id }  = event.target.parentElement;
+
+  $(event.target).toggleClass('lock-closed');
+
+  if(colorBoxes.getBoxes().includes(id)) {
+    colorBoxes.lock(id)
+  } else {
+    colorBoxes.unlock(id)
+  }
 };
 
