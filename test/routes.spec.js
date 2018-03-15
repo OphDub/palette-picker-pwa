bd7ef62cae07a456e74a7b09063407ee9bcb56ee
Ophus Wong <ophus.wong@gmail.com>
2018-03-15 14:26:40 -0600

    Add client route tests complete

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,6 +5,27 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
+describe('Client Routes', () => {
+  it('should return the home page', () => {
+    return chai.request(server)
+    .get('/')
+    .then(response => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it('should return a 404 for a route that does not exist', () => {
+    return chai.request(server)
+    .get('/sad')
+    .then(response => {
+      expect(response.status).to.equal(404);
+    })
+    .catch(error => {
+      throw error;
+    });
+  });
+});
+
 describe('API Routes', () => {
   describe('GET /api/v1/projects', () => {
     it('should return all the projects', () => {
@@ -14,10 +35,11 @@ describe('API Routes', () => {
         expect(response.status).to.equal(200);
         expect(response).to.be.json;
         expect(response.body).to.be.an('array');
+        console.log(response.body);
       })
       .catch(error => {
         throw error;
-      })
+      });
     });
   });
 });
